25ce8cb2fdc596c84ddd26a9dd84a3a1c1125311
TealSmurf <jcoin707@gmail.com>
2024-03-09 02:48:08 -0800

    Created program that takes user input repeatedly and prints that input until current input is > previous input.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,25 @@ using namespace std;
 int main()
 {
 	int prev, current;
-	/* ****************************
-	// Complete this code.
-	*************************** */
+    bool a1 = true;
+
+    while (true) {
+        int current;
+        cout << "Enter an integer: ";
+        cin >> current;
+
+        if (a1) {
+            prev = current;
+            a1 = false;
+        } else {
+            if (current < prev) {
+                cout << current << " ";
+            } else {
+                cout << endl;
+                break;
+            }
+        }
+
+        prev = current;
+    }
 }
